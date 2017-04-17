81c6f6fd7d41b1a1c00a8a68147f4d7a51017e88
ollie-dev <oliver.kuhl@gmail.com>
2017-04-17 13:21:19 +0200

    Added further config properties

--- src/main/java/in/ollie/innogysmarthome/entity/ConfigPropertyList.java
+++ src/main/java/in/ollie/innogysmarthome/entity/ConfigPropertyList.java
@@ -16,15 +16,38 @@ public abstract class ConfigPropertyList extends PropertyList {
 
     /** config property names */
     protected static final String CONFIG_PROPERTY_NAME = "Name";
+    protected static final String CONFIG_PROPERTY_PROTOCOL_ID = "ProtocolId";
     protected static final String CONFIG_PROPERTY_TIME_OF_ACCEPTANCE = "TimeOfAcceptance";
     protected static final String CONFIG_PROPERTY_TIME_OF_DISCOVERY = "TimeOfDiscovery";
     protected final static String CONFIG_PROPERTY_HARDWARE_VERSION = "HardwareVersion";
     protected final static String CONFIG_PROPERTY_SOFTWARE_VERSION = "SoftwareVersion";
     protected final static String CONFIG_PROPERTY_FIRMWARE_VERSION = "FirmwareVersion";
     protected final static String CONFIG_PROPERTY_HOSTNAME = "HostName";
+    protected final static String CONFIG_PROPERTY_ACTIVITY_LOG_ENABLED = "ActivityLogEnabled";
+    protected final static String CONFIG_PROPERTY_CONFIGURATION_STATE = "ConfigurationState";
+    protected final static String CONFIG_PROPERTY_GEOLOCATION = "GeoLocation";
+    protected final static String CONFIG_PROPERTY_TIMEZONE = "TimeZone";
+    protected final static String CONFIG_PROPERTY_CURRENT_UTC_OFFSET = "CurrentUTCOffset";
     protected final static String CONFIG_PROPERTY_IP_ADDRESS = "IPAddress";
     protected final static String CONFIG_PROPERTY_MAC_ADDRESS = "MACAddress";
+    protected final static String CONFIG_PROPERTY_SHC_TYPE = "ShcType";
+    protected final static String CONFIG_PROPERTY_BACKEND_CONNECTION_MONITORED = "BackendConnectionMonitored";
+    protected final static String CONFIG_PROPERTY_RFCOM_FAILURE_NOTIFICATION = "RFCommFailureNotification";
+    protected final static String CONFIG_PROPERTY_POSTCODE = "PostCode";
+    protected final static String CONFIG_PROPERTY_CITY = "City";
+    protected final static String CONFIG_PROPERTY_STREET = "Street";
+    protected final static String CONFIG_PROPERTY_HOUSENUMBER = "HouseNumber";
+    protected final static String CONFIG_PROPERTY_COUNTRY = "Country";
+    protected final static String CONFIG_PROPERTY_HOUSEHOLD_TYPE = "HouseholdType";
+    protected final static String CONFIG_PROPERTY_NUMBER_OF_PERSONS = "NumberOfPersons";
+    protected final static String CONFIG_PROPERTY_NUMBER_OF_FLOORS = "NumberOfFloors";
+    protected final static String CONFIG_PROPERTY_LIVINGAREA = "LivingArea";
     protected final static String CONFIG_PROPERTY_REGISTRATION_TIME = "RegistrationTime";
+    protected final static String CONFIG_PROPERTY_DISPLAY_CURRENT_TEMPERATURE = "DisplayCurrentTemperature";
+    protected final static String CONFIG_PROPERTY_UNDERLYING_DEVICE_IDS = "UnderlyingDeviceIds";
+
+    protected final static String PROTOCOL_ID_COSIP = "Cosip";
+    protected final static String PROTOCOL_ID_VIRTUAL = "Virtual";
 
     /**
      * This represents a container of all configuration properties.
@@ -92,6 +115,10 @@ public abstract class ConfigPropertyList extends PropertyList {
         return getPropertyValueAsString(CONFIG_PROPERTY_NAME);
     }
 
+    public String getProtocolId() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_PROTOCOL_ID);
+    }
+
     /**
      * Returns the time, when the {@link Device} was added to the SHC configuration.
      *
@@ -136,12 +163,80 @@ public abstract class ConfigPropertyList extends PropertyList {
         return getPropertyValueAsString(CONFIG_PROPERTY_HOSTNAME);
     }
 
+    public Boolean getActivityLogEnabled() {
+        return getPropertyValueAsBoolean(CONFIG_PROPERTY_ACTIVITY_LOG_ENABLED);
+    }
+
+    public String getConfigurationState() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_CONFIGURATION_STATE);
+    }
+
+    public String getGeoLocation() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_GEOLOCATION);
+    }
+
+    public String getTimeZone() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_TIMEZONE);
+    }
+
+    public Integer getCurrentUTCOffset() {
+        return getPropertyValueAsInteger(CONFIG_PROPERTY_CURRENT_UTC_OFFSET);
+    }
+
     public String getIpAddress() {
         return getPropertyValueAsString(CONFIG_PROPERTY_IP_ADDRESS);
     }
 
     public String getMacAddress() {
         return getPropertyValueAsString(CONFIG_PROPERTY_MAC_ADDRESS);
+    }
+
+    public String getSHCType() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_SHC_TYPE);
+    }
+
+    public Boolean getBackendConnectionMonitored() {
+        return getPropertyValueAsBoolean(CONFIG_PROPERTY_BACKEND_CONNECTION_MONITORED);
+    }
+
+    public Boolean getRFCommFailureNotification() {
+        return getPropertyValueAsBoolean(CONFIG_PROPERTY_RFCOM_FAILURE_NOTIFICATION);
+    }
+
+    public String getPostCode() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_POSTCODE);
+    }
+
+    public String getCity() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_CITY);
+    }
+
+    public String getStreet() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_STREET);
+    }
+
+    public String getHouseNumber() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_HOUSENUMBER);
+    }
+
+    public String getCountry() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_COUNTRY);
+    }
+
+    public String getHouseHoldType() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_HOUSEHOLD_TYPE);
+    }
+
+    public Integer getNumberOfPersons() {
+        return getPropertyValueAsInteger(CONFIG_PROPERTY_NUMBER_OF_PERSONS);
+    }
+
+    public Integer getNumberOfFloors() {
+        return getPropertyValueAsInteger(CONFIG_PROPERTY_NUMBER_OF_FLOORS);
+    }
+
+    public Integer getLivingArea() {
+        return getPropertyValueAsInteger(CONFIG_PROPERTY_LIVINGAREA);
     }
 
     public DateTime getRegistrationTime() {
@@ -151,5 +246,13 @@ public abstract class ConfigPropertyList extends PropertyList {
     public String getRegistrationTimeFormattedString() {
         return Util.convertZuluTimeStringToDate(getPropertyValueAsString(CONFIG_PROPERTY_REGISTRATION_TIME))
                 .toString(Constants.FORMAT_DATETIME);
+    }
+
+    public String getDisplayCurrentTemperature() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_DISPLAY_CURRENT_TEMPERATURE);
+    }
+
+    public String getUnderLyingDeviceIds() {
+        return getPropertyValueAsString(CONFIG_PROPERTY_UNDERLYING_DEVICE_IDS);
     }
 }
